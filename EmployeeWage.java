b888fbac8862cd15368297aa30d423e0ee7343ae
mouna07 <mounamc261@gmail.com>
2021-02-01 12:02:51 +0530

    Added: Part time employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,13 +1,23 @@
+import java.util.Random;
 class EmployeeWage{
 	public static void main(String args[]){
 		System.out.println("Welocome to the Employee Wage problem");
+		Random random=new Random();
 		int isFullTime=1;
+		int isPartTime=2;
 		int wagePerHour=20;
-		int fullDayHour=8;
-		double empcheck=Math.floor(Math.random()*10)%2;
+		int fullTimeHour=8;
+		int partTimeHour=4;
+		int employeeWage;
+		double empcheck=random.nextInt(3);
 		if (empcheck==isFullTime){
-			System.out.println("Employee is present");
-			int employeeWage = wagePerHour*fullDayHour;
+			System.out.println("Employee is present and full time employee");
+			employeeWage = wagePerHour*fullTimeHour;
+			System.out.println("employee wage is:"+employeeWage);
+		}
+		else if(empcheck==isPartTime){
+			System.out.println("Employee is present but part time employee");
+			employeeWage = wagePerHour*partTimeHour;
 			System.out.println("employee wage is:"+employeeWage);
 		}
 		else
