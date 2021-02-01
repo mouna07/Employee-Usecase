afeb2fdee12fd6b1de37b32f6ae4ac8002c6fb1d
mouna07 <mounamc261@gmail.com>
2021-02-01 11:00:33 +0530

    Added: employee check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,12 @@
 class EmployeeWage{
 	public static void main(String args[]){
 		System.out.println("Welocome to the Employee Wage problem");
+		int isFullTime=1;
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if (empcheck==isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
 
